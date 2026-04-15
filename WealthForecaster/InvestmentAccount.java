11f0e62f5070169c90c140f1fa7b59108fbc6315
eeshapenkey3-cpu <eeshapenkey3@gmail.com>
2026-04-15 07:59:41 +0100

    made private variables, set and get methods for investmentAccount java

---
+++ WealthForecaster/InvestmentAccount.java
@@ -0,0 +1,29 @@
+public class InvestmentAccount{
+    //private data members
+    private double currentBalance, annualContribution, expectedReturnRate;
+
+    //constructor
+    public InvestmentAccount(){
+        currentBalance = 0;
+        annualContribution = 0;
+        expectedReturnRate = 0;
+    }
+
+    //set methods - one for each input
+    public void setCurrentBalance(double currentBalance){
+        this.currentBalance = currentBalance;
+    }    
+
+    public void setAnnualContribution(double annualContribution){
+        this.annualContribution = annualContribution;
+    }
+
+    public void setEexpectedRerunRate(double expectedReturnRate){
+        if(expectedReturnRate) >= 1.0{
+            this.expectedReturnRate = expectedReturnRate / 100;
+        }else{
+            this.expectedReturnRate = expectedReturnRate;
+        }
+    }
+
+}
